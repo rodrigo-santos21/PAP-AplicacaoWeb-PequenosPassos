b8c3f447444766072fc178518f39f04e01f46cc4
Rodrigo Santos <rodrigoemanuelsantos2@gmail.com>
2026-01-04 12:05:39 +0000

    Atualização no admin.php e adição e eliminação de ficheiros
    No admin.php atualizei os botões, por uma questão de organização. Removi os ficheiros de remoção e adicionei alguns ficheiros que ainda estavam em falta.

--- admin.php
+++ admin.php
@@ -19,27 +19,18 @@ if (!isset($_SESSION['tipo']) || $_SESSION['tipo'] !== 'administrador') {
         </h2>
 
         <nav class="space-y-4">
-            <a href="listarutl.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Lista Utilizadores</a>
-            <a href="logs.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Consultar Logs</a>
             <a href="adicionarutl.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Adcionar utilizador</a>
-            <a href="editarutl.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Editar utilizador</a>
-            <a href="removerutl.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Eliminar utilizador</a>
+            <a href="listarutl.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Lista Utilizadores</a>
             <a href="adicionaratv.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Adcionar atividade</a>
-            <a href="editaratv.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Editar atividade</a>
-            <a href="removeratv.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Eliminar atividade</a>
+            <a href="listaratv.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Listar Atividade</a>
             <a href="adicionarreu.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Adcionar Reunião</a>
-            <a href="editarreu.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Editar Reunião</a>
-            <a href="removerreu.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Remover Reuião</a>
+            <a href="listarreu.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Listar Reunião</a>
             <a href="adicionaroco.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Marcar ocorrência</a>
-            <a href="editaroco.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Editar ocorrência</a>
-            <a href="removeroco.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Remover ocorrência</a>
+            <a href="listaroco.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Listar Ocorrência</a>
             <a href="adicionarcri.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Adcionar Criança</a>
-            <a href="editarcri.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Editar Criança</a>
-            <a href="removercri.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Remover Criança</a>
             <a href="listacri.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Listar Criança</a>
             <a href="listapre.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Histórico de Presenças</a>
-            <a href="editarpre.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Editar Presença</a>
-            <a href="removerpre.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Remover Presença</a>
+            <a href="logs.php" class="block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Consultar Logs</a>
         </nav>
 
         <form method="post" action="logout.php" class="mt-6">
